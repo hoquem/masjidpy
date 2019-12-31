42d6ee9acc0dd107492ef25d177aca2ce118237f
Mahmudul Hoque <m.hoque@gmail.com>
2019-12-31 15:29:27 +0000

    login - not working

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-load_dotenv(os.path.join(basedir, 'env'))
+#basedir = os.path.abspath(os.path.dirname(__file__))
+#load_dotenv(os.path.join(basedir, 'env'))
 
 class Config(object):
-    #...
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'some-secret-key-you-will-probably-guess'
