9239a4e8034877aa07e21d95b625e2411bd87d96
Mahmudul Hoque <m.hoque@gmail.com>
2019-12-31 13:23:49 +0000

    basic

--- app/routes.py
+++ app/routes.py
@@ -6,4 +6,27 @@ from app import app
 def index():
     user = {'username': 'Doods'}
 
-    return render_template('index.html', title='Home', user=user)
+    members = [
+        {
+            'name': 'Mahmudul Hoque',
+            'address': {
+                'street': "22 Carlton Close",
+                'city': 'Luton',
+                'postcode': 'LU3 1ER'
+            },
+            'mobile': '07961 103217',
+            'email': 'm.hoque@gmail.com'
+        },
+        {
+            'name': 'Ubaid Hoque',
+            'address': {
+                'street': "22 Carlton Close",
+                'city': 'Luton',
+                'postcode': 'LU3 1ER'
+            },
+            'mobile': '07961 1111111',
+            'email': 'u.hoque@gmail.com'
+        }
+    ]    
+
+    return render_template('index.html', title='Home', user=user, members=members)
